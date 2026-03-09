c19aa00be2bf88c61b7f64133cc4d940a6c05544
Filip Nyhamn <filnyh09@gvgs.no>
2026-03-09 18:57:08 +0100

    Implement player control and position update in the main game loop

--- __main__.py
+++ __main__.py
@@ -1,22 +1,50 @@
+#librery imports
+import sys
 import pygame
+import time
 from random import randint
 
+#snake class import
 from snake import snake
-
 player = snake(400, 300)
 
+#initilise game
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.event.get()
 
+#seting veriables
+pos_update_time = time.time()
+
+
+#main loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
+        
+        player.control(event)
+
+    if time.time() - pos_update_time > 0.5:
+        player.pos_update()
+        pos_update_time = time.time()
+
+
+    #print(f"dir_x: {player.dir_x}, dir_y: {player.dir_y}")
+    #print(f"x: {player.x} y: {player.y}")
+
+    #screan render
     screen.fill((255, 255, 255))
     player.draw(screen)
     pygame.display.update()
 
+    #emergency exit
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LSHIFT] and keys[pygame.K_LCTRL]:
+        print("[emergency exit successfull]")
+        break
+
+
 pygame.quit()
+sys.exit("[exit successfull]")
--- snake.py
+++ snake.py
@@ -6,5 +6,24 @@ class snake:
         self.x = int(x)
         self.y = int(y)
         self.size = int(50)
+        self.dir_x = int(0)
+        self.dir_y = int(0)
     def draw(self, screen):
         pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(self.x, self.y, self.size, self.size))
+    def control(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT and self.dir_x != 1:
+                self.dir_x = -1
+                self.dir_y = 0
+            elif event.key == pygame.K_RIGHT and self.dir_x != -1:
+                self.dir_x = 1
+                self.dir_y = 0
+            elif event.key == pygame.K_UP and self.dir_y != 1:
+                self.dir_x = 0
+                self.dir_y = -1
+            elif event.key == pygame.K_DOWN and self.dir_y != -1:
+                self.dir_x = 0
+                self.dir_y = 1
+    def pos_update(self):
+        self.x += self.dir_x * self.size
+        self.y += self.dir_y * self.size
